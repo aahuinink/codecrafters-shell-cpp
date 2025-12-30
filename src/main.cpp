da004da810c5ff470a6aa830e8b0993f693d5782
Aaron Huinink - Surface Book <a_huinink@hotmail.ca>
2025-12-30 06:55:44 -0800

    created error handler class

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <string>
+#include "error_handling.h"
+
 
 int main() {
-  // Flush after every std::cout / std:cerr
-  std::cout << std::unitbuf;
-  std::cerr << std::unitbuf;
 
-  // print the prompt
-  std::cout << "$ ";
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+
+    ErrorHandler err_handler;
+
+    // print the prompt
+    std::cout << "$ ";
+
+    // get user input
+    std::string user_input;
+    std::getline(std::cin, user_input);
+
+    err_handler.handle_error(ErrorHandler::Conditions(ErrType::INVALID_COMMAND, user_input));
+
 }
