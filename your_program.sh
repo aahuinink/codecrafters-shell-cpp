8005315516655fb6c9cbd28767b48c5d0f6ad80b
Aaron Huinink - Surface Book <a_huinink@hotmail.ca>
2026-03-04 11:50:19 -0800

    added rm -rf build to codecrafter script

--- your_program.sh
+++ your_program.sh
@@ -14,6 +14,7 @@ set -e # Exit early if any commands fail
 # - Edit .codecrafters/compile.sh to change how your program compiles remotely
 (
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
+  rm -rf build
   cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
   cmake --build ./build
 )
